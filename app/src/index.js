4173e005a81ad9d8b83b0ecc22f8e9ed23dbcf7e
Tommy Grullon <tommycontreras34@gmail.com>
2026-08-27 11:20:35 -0400

    refactor: improve code formatting and consistency in index.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,23 @@
-import express from "express"
-import config from "./../config/index.js"
+import express from "express";
+import config from "./../config/index.js";
 
-const app = express()
+const app = express();
+
+app.use(express.json())
 
 app.get("/", (_req, res) => {
-  res.status(200).send("Hello, World!")
-})
+  res.status(200).send("Hello, World!");
+});
 
-app.listen((config.PORT), () => {
-  console.log(`The server is running on port ${config.PORT}`)  
-})
+app.post("/secret", (req, res) => {
+  const { user, password } = req.body;
+
+  if (user != config.USERNAME || password != config.PASSWORD)
+    res.status(400).json({ message: "User y/o password incorrect" });
+
+  res.status(200).json({ message: config.SECRET_MESSAGE });
+});
+
+app.listen(config.PORT, () => {
+  console.log(`The server is running on port ${config.PORT}`);
+});

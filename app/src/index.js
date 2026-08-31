9590d6ca2d6e4bdbd396b5d72c2dcaab02b311d3
Tommy Grullon <tommycontreras34@gmail.com>
2026-08-31 10:49:31 -0400

    fix: update authentication method for /secret route to use Basic Auth

--- app/src/index.js
+++ app/src/index.js
@@ -9,13 +9,31 @@ app.get("/", (_req, res) => {
   res.status(200).send("Hello, World!");
 });
 
-app.post("/secret", (req, res) => {
-  const { user, password } = req.body;
+app.get("/secret", (req, res) => {
+  const auth = req.headers.authorization
 
-  if (user !== config.USERNAME || password !== config.PASSWORD)
-    res.status(401).json({ message: "User y/o password incorrect" });
+  if(!auth?.startsWith("Basic "))
+    return res
+      .status(401)
+      .set("WWW-Authenticate", 'Basic realm="Restricted"')
+      .send("Authentication required")
 
-  res.status(200).json({ message: config.SECRET_MESSAGE });
+  const encodedCredentials = auth.split(" ")[1]
+  const credentials = Buffer.from(encodedCredentials, "base64").toString("utf8")
+
+  const [user, password] = credentials.split(":")
+
+  if (
+    user !== config.USERNAME ||
+    password !== config.PASSWORD
+  ) {
+    return res
+      .status(401)
+      .set("WWW-Authenticate", 'Basic realm="Restricted"')
+      .send("Invalid username or password");
+  }
+
+  return res.status(200).send(config.SECRET_MESSAGE);
 });
 
 app.listen(config.PORT, () => {
